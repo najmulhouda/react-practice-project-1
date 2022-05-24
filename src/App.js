8a564b8f195e671f7b61d4ed4ef67c01863c7a64
najmulhouda <najmulhouda6@gmail.com>
2022-05-24 16:14:42 +0600

    practice react commponent and other things

--- src/App.js
+++ src/App.js
@@ -2,6 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const number = 555
+  const singer = {
+    name: 'Dr, Mahfuz',
+    job: 'Singer',
+  }
+  const singer2 = {
+    name:'Eva Rahman',
+    job:'kokil konthi gaan er pakhi'
+  }
+  const singerStyle = {
+    backgroundColor: 'black',
+    color: 'white',
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,14 +26,25 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="container">
+        <p>my number is : {number}</p>
+        <p style={
+          {
+            backgroundColor: 'black',
+            color: 'white',
+          }
+        }>Singer: {singer.name + " " + singer.job}</p>
+        <p style={singerStyle}>Real singer: {singer2.job}</p>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React And other things
         </a>
+        <p>hululu</p>
       </header>
     </div>
   );
